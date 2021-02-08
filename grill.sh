e27021327d1ddfb6988ad8c194a9d81f788b18b9
Tomasz Cygal <tomasz.cygal@ibm.com>
2021-02-08 18:14:38 +0100

    new folder change with -

--- grill.sh
+++ grill.sh
@@ -3,8 +3,8 @@
 
 for serwer in $@; 
 
-do /bin/echo -e "***********" && echo $serwer && /bin/echo -e "OFFSET - " | tr -d '\n'  && ssh -T $serwer < /home/plzz030n/scripts/offset.sh | tr -d '\n' &&  /bin/echo  "  " ;
-/bin/echo -e "\nCROSS - " | tr -d '\n' && ssh -T $serwer < /home/plzz030n/scripts/cross.sh |  tr -d '\n' &&  /bin/echo  "  " ; ssh -T $serwer < /home/plzz030n/scripts/fsy.sh
+do /bin/echo -e "***********" && echo $serwer && /bin/echo -e "OFFSET - " | tr -d '\n'  && ssh -T $serwer < /Users/tomaszcygal/Documents/Scripts/offset.sh | tr -d '\n' &&  /bin/echo  "  " ;
+/bin/echo -e "\nCROSS - " | tr -d '\n' && ssh -T $serwer < /Users/tomaszcygal/Documents/Scripts/cross.sh |  tr -d '\n' &&  /bin/echo  "  " ; ssh -T $serwer < /Users/tomaszcygal/Documents/Scripts/fsy.sh
 
 
 done
